1b6b7715ed5830eb053d1045de34f7c86f70920c
justinrobinson <justinrobinson@bizjournals.com>
2016-02-26 08:59:31 -0500

    [master] require a scanner to be passed to the struct

--- csv-parser.go
+++ csv-parser.go
@@ -2,41 +2,23 @@ package csv_parser
 
 import (
 	"bufio"
-	"os"
 	"strings"
 )
 
-type CsvParser struct {}
+type CsvParser struct {
+	scanner *bufio.Scanner
+}
 
 
-func parse(scanner *bufio.Scanner) ([][]string) {
+func (c *CsvParser) Parse() ([][]string) {
 
 	// where are parsed lines will be stored
 	parsed := [][]string{}
 
 	// add each line to our parsed array
-	for scanner.Scan() {
-		parsed = append(parsed, strings.Split(scanner.Text(), ","))
+	for c.scanner.Scan() {
+		parsed = append(parsed, strings.Split(c.scanner.Text(), ","))
 	}
 
 	return parsed
-}
-
-func (c *CsvParser) ParseFile(fileName string) ([][]string, error) {
-
-	// open file
-	file, err:= os.Open(fileName)
-
-	// return err if we have one
-	if err != nil {
-		return [][]string{}, err
-	}
-
-	return parse(bufio.NewScanner(file)), nil
-}
-
-
-func (c *CsvParser) ParseString(input string) ([][]string) {
-
-	return parse(bufio.NewScanner(strings.NewReader(input)))
 }
